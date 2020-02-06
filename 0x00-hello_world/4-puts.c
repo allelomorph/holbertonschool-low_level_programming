c47e96906a09522d92e0b12b94ced8fd50d3708b
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-06 03:03:23 +0000

    further matching of betty standard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: Always returns 0 - indicates success
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
