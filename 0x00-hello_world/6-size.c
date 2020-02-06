efb227790b92e8956c4103409a70da1d69eeba56
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-06 03:45:19 +0000

    edits to parentheses sytnax, changed all variables to lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %dl byte(s)\n", sizeof(char);
-printf("Size of a int: %dl byte(s)\n", sizeof(int);
-printf("Size of a long int: %dl byte(s)\n", sizeof(long int);
-printf("Size of a long long int: %dl byte(s)\n", sizeof(long long int);
-printf("Size of a float: %dl byte(s)\n", sizeof(float);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
