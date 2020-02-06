aa37cadcb6a742d3587f86c388f2c6aa137a0770
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-06 03:31:09 +0000

    first try 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of a char: %dl byte(s)\n", sizeof(char);
+printf("Size of a int: %dl byte(s)\n", sizeof(int);
+printf("Size of a long int: %dl byte(s)\n", sizeof(long int);
+printf("Size of a long long int: %dl byte(s)\n", sizeof(long long int);
+printf("Size of a float: %dl byte(s)\n", sizeof(float);
+return (0);
+}
