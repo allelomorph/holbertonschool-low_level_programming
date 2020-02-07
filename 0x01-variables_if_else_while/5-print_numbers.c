c2c300092123eaf4058fcbfd9203ba3dd24ea96c
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-07 01:32:58 +0000

    first try with printf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: return 0 to complete
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
