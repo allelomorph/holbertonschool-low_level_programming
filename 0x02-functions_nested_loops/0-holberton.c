579a11c45d0009d77807c943074af22a1ff077ea
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-11 02:12:26 +0000

    first try without array

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - entry point
+ *
+ * Return: return 0 to complete
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
