2308ca23c81a259804051603591962feb69e4ac3
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-11 21:19:37 +0000

    2nd draft fo 10 without sum variable, updated header protos

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result.
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: sum of both integer args
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -24,5 +24,9 @@ void jack_bauer(void);
 
 void times_table(void);
 
+int add(int, int);
+
+void print_to_98(int n);
+
 /* closes double inclusion protection */
 #endif
