3ad61d8fcf72f03ee6d1689104d4347f1af2d333
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-12 03:37:17 +0000

    simple solutions first

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - tests positive_or_negative for correct output with 0 as input
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
