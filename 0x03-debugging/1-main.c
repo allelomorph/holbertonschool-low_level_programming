34dd96bc2ecd22d476150abe549266c5101c8643
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-12 04:25:21 +0000

    first try, simple block comment

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop (remedy in comment)
+ *
+ * @i: integer intended to drive loop
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+/**
+ *	while (i < 10)
+ *	{
+ *		putchar(i);
+ *	}
+ */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
