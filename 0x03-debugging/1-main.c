d2cf522793add7a3028501d7014d576012f19d9d
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-12 07:37:32 +0000

    changed wc -l line count

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	/*
@@ -21,6 +20,5 @@ int main(void)
 	* }
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
