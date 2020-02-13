c3872ee1380c2c6e3b9442dc7db3a319b7b9dee8
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-13 18:04:50 +0000

    first push of dir and empty files

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/* 0x03. C - Debugging */
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+/* 0x04. C - More functions, more nested loops */
+
+int _isupper(int c);
+
+#endif /* HOLBERTON_H */
