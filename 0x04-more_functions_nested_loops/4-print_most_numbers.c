de474be779df02aeec58350d5bcbdb387cbb81be
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-13 21:33:42 +0000

    first try, single if with or operator in condition

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9, except 2 and 4, then new line
+ *
+ * Return: none
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
