36be560eed34f923ad7b007df0ae30379db0afdf
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-13 18:40:18 +0000

    mod of 0x02, task 10 "a + b"

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers and returns the result
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: product of both integer args
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
