ceb9d8b620bd2660a8af79baac0b85a2b15e6f18
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-14 03:09:54 +0000

    more elegant solutions exist I'm sure

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 when complete
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf(" Fizz");
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+		if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			if (i > 1)
+			{
+				printf(" ");
+			}
+			printf("%i", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
