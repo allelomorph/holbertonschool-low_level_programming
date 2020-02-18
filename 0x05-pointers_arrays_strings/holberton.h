3be489a2ef5ae1148a9e4aa5b091ee3b0f703147
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-18 00:51:03 +0000

    simple dereference for task 0, added header

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,89 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/* above opens double inclusion protection */
+
+#include <unistd.h>
+
+/* 0x02. C - Functions, nested loops */
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+/* 0x03. C - Debugging */
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+/* 0x04. C - More functions, more nested loops */
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+/* 0x05. C - Pointers, arrays and strings */
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+/* closes double inclusion protection */
+
+#endif /* HOLBERTON_H */
