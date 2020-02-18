1f4d70edaa86079e00fc01facbfcdc6dd4364b9f
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-18 00:51:03 +0000

    simple dereference for task 0, added header

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates the int value to 98
+ *
+ * @n: integer value given to function
+ *
+ * return - void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
