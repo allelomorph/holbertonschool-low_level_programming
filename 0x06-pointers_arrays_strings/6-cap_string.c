da232ed1b75171c607a1e418eb06ddb6578b3ed4
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-21 03:12:30 +0000

    correct outputs, one line possibly too long for Betty

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words in a string. valid word
+ * separators include: ' ', '\t', '\n', ',', ';', '.', '!', '?',
+ * '"', '(', ')', '{', and '}'
+ *
+ * @stg: in this definiton, name of string to be modified,
+ *       but in protoype dedclaration, it is unnamed
+ *
+ * Return: modified string
+ */
+
+char *cap_string(char *stg)
+{
+	int i;
+	int d;
+	char *delim = " \t\n,;.!?\"(){}";
+
+	printf("%s\n", delim);
+
+/* incrementing to end of stg */
+	for (i = 0; stg[i] != 0; i++)
+	{
+/* incrementing through string of delimiter chars */
+		for (d = 0; delim[d] != 0; d++)
+		{
+/* check if current postiion in stg is a delimiter */
+			if (stg[i] == delim[d])
+			{
+/* test for lowercase alphabet ASCII values */
+				if ((stg[(i + 1)] >= 'a') && (stg[(i + 1)] <= 'z'))
+				{
+/* capitalize by dropping ASCII value, indicentally, difference = ' ' */
+					stg[(i + 1)] = (stg[(i + 1)] - 32);
+				}
+			}
+		}
+	}
+	return (stg);
+}
