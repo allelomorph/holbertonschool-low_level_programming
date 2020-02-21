3c3315954650a5f69a186a084279f2f4ddae1198
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-21 04:07:30 +0000

    trying for economy of if gates

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes a string into classsic 1337speak
+ *
+ * @stg: in this definiton, name of string to be modified,
+ *       but in protoype dedclaration, it is unnamed
+ *
+ * Return: modified string
+ */
+
+char *leet(char *stg)
+{
+	int i;
+	int j;
+
+	char *rem = "aAeEoOtTlL";
+	char *rep = "43071";
+
+/* incrementing to end of stg */
+	for (i = 0; stg[i] != 0; i++)
+	{
+/* incrementing through string of replaceable chars */
+		for (j = 0; rem[j] != 0; j++)
+		{
+/* check if current postiion in stg is a replaceable char */
+			if (rem[j] == stg[i])
+			{
+/* replace with corresponding char from 1337 string */
+				stg[i] == rep[(j / 2)];
+				break;
+			}
+		}
+	}
+	return (stg);
+}
