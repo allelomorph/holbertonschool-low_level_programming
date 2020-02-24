b3992ef8e2c17f348c30e5167385f61c91735265
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-24 22:34:18 +0000

    switched to pointer notation

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,10 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i]; i++)
+	for (; *s; s++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (*s == c)
+			return (s);
 	}
-	return ('\0');
+	return (NULL);
 }
