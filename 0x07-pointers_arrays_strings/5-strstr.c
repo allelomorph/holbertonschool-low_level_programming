523495824d80c6771225befc4553eb025ae015ad
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-25 05:34:43 +0000

    3rd major rebuild, see comments

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,33 +15,82 @@
  */
 
 char *_strstr(char *haystack, char *needle)
+/* 3rd draft after 2nd cosult */
 {
 	int i;
-	int match;
 
-	for (; *haystack; haystack++)
-	{
-		for (i = 0; *(needle + i); i++)
-		{
-			if (*(needle + i) == 0)
-			{
-				return (0);
-			}
-			if (*(haystack + i) == *(needle + i))
-			{
-				match = 1;
-				continue;
-			}
-			else
-			{
-				match = 0;
-				break;
+ 	for (; *haystack; haystack++)
+ 	{
+ 		for (i = 0; *(needle + i); i++)
+ 		{
+ 			if (*(needle + i) != *(haystack + i))
+ 			{
+ 				break;
 			}
 		}
-		if (match)
+ 		if (!*(needle + i))
 		{
 			return (haystack);
-		}
-	}
-	return (0);
+ 		}
+ 	}
+ 	return (NULL);
 }
+
+/*
+ * edit after Tabs consult
+ *	int i;
+ *
+ *	for (; *haystack; haystack++)
+ *	{
+ *		for (i = 0; *(needle + i); i++)
+ *		{
+ *			if (!(*(haystack + i)))
+ *			{
+ *				return (0);
+ *			}
+ *			if (*(haystack + i) != *(needle + i))
+ *			{
+ *				break;
+ *			}
+ *			if (*(needle + i) == 0)
+ *			{
+ *				return (haystack);
+ *			}
+ *		}
+ *	}
+ *	return (0);
+ *}
+ */
+
+/*
+ * first draft, missed one output check
+ *	int i;
+ *	int match;
+ *
+ *	for (; *haystack; haystack++)
+ *	{
+ *		for (i = 0; *(needle + i); i++)
+ *		{
+ *			if (*(needle + i) == 0)
+ *			{
+ *				return (0);
+ *			}
+ *			if (*(haystack + i) == *(needle + i))
+ *			{
+ *				match = 1;
+ *				continue;
+ *			}
+ *			else
+ *			{
+ *				match = 0;
+ *				break;
+ *			}
+ *		}
+ *		if (match)
+ *		{
+ *			return (haystack);
+ *		}
+ *	}
+ *	return (0);
+ *}
+ */
