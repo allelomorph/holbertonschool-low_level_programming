002b2fae78ba25819811129328ffbadb5a31081d
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-25 06:33:57 +0000

    betty compliance edits

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,21 +19,21 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 
- 	for (; *haystack; haystack++)
- 	{
- 		for (i = 0; *(needle + i); i++)
- 		{
- 			if (*(needle + i) != *(haystack + i))
- 			{
- 				break;
+	for (; *haystack; haystack++)
+	{
+		for (i = 0; *(needle + i); i++)
+		{
+			if (*(needle + i) != *(haystack + i))
+			{
+				break;
 			}
 		}
- 		if (!*(needle + i))
+		if (!*(needle + i))
 		{
 			return (haystack);
- 		}
- 	}
- 	return (NULL);
+		}
+	}
+	return (NULL);
 }
 
 /*
