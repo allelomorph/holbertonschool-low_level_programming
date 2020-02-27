8a5c336ec347b309b3c134c6cacec3bb63fce4d5
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-27 04:34:09 +0000

    moved recursion to helper function after copious help from peeers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * find_sq - recursive helper of _sqrt_recursion, adds second variable to
+ * increment guesses to compare against n
+ *
+ * @guess: sucessive candiadates for square root of n
+ *
+ * @n: number to be checked for root
+ *
+ * Return: natural sqaure root of n, or -1 if none found
+ */
+
+int find_sq(int guess, int n)
+{
+	if (guess * guess == n)
+		return (guess);
+	else if (guess * guess < n)
+		return (find_sq(guess + 1, n));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - finds natural square root of n through recursive helper
+ *
+ * @n: number to be checked
+ *
+ * Return: natural sqaure root of n, or -1 if none found
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (find_sq(2, n));
+}
