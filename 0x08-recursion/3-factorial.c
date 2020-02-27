28eb6a3d2a473cf10c93274cdddc267dd3fa4f96
Samuel Pomeroy <1690@holbertonschool.com>
2020-02-27 00:54:54 +0000

    first try

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * factorial - finds factorial of int n.
+ * if n is less than 0, return -1 for error
+ *
+ * @n: base number to derive factorial
+ *
+ * Return: factorial of n, or -1 if n < 0
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
