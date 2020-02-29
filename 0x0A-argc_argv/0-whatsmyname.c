6a4b34dd9e91ae8a472b5b99d96cee70a2a09941
Samuel Pomeroy <1960@holbertonschool.com>
2020-02-29 23:07:28 +0000

    first try

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints file name of program
+ *
+ * @argc: amount of cmd line arguments
+ *
+ * @argv: array of strings containing cmd line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
