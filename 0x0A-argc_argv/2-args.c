8d4a0ebc6c93fbba20eadfac7bcb4708cb3fa120
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-01 00:46:17 +0000

    tried both iteratively and recursively

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints all arguments it receives
+ *
+ * @argc: amount of cmd line arguments, starting at 0
+ *
+ * @argv: array of strings containing cmd line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+/*
+ * Done iteratively here. Is recursion possible if argc and argv
+ * wouldn't be taking from standard input on future stack frames?
+ */
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
