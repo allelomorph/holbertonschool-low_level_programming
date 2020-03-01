7405ecd67ca4b6f796a5ec9d50677c5eca9da082
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-01 01:32:48 +0000

    passes local tests

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point, multiplies two numbers, returns 1 and prints
+ * "Error" if not receiving 2 arguments
+ *
+ * @argc: amount of cmd line arguments, starting at 0 for program
+ * name itself
+ *
+ * @argv: array of strings containing cmd line arguments
+ *
+ * Return: 0 on sucess, 1 if incorrect number of arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		prod = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", prod);
+		return (0);
+	}
+}
