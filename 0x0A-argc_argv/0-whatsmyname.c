50eb2d6e0e7b4bf2200e03489d5bca23ddc0b4d9
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-01 00:52:08 +0000

    change to description of @argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,8 @@
 /**
  * main - entry point, prints file name of program
  *
- * @argc: amount of cmd line arguments
+ * @argc: amount of cmd line arguments, starting at 0 for program
+ * name itself
  *
  * @argv: array of strings containing cmd line arguments
  *
