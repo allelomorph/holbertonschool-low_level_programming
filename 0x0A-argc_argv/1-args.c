ffb952c74cf88235eef13ab88b52aef2d5f67ff3
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-02 05:05:21 +0000

    incorporated voiding of argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,15 +13,16 @@
 int main(int argc, char *argv[])
 {
 /*
- * Needed to express argc as pointer arithmetic due to strictest
- * gcc flags requiring 0 or 2 arguments to main(), and not allowing
- * an unused argument to main.
+ * Originally needed to express argc as pointer arithmetic due to
+ * strictest gcc flags requiring 0 or 2 arguments to main(), and not
+ * allowing an unused argument to main.
+ * First cleared checks with:
+ *	int n = (((argv + argc) - argv) - 1);
+ *	printf("%d\n", n);
  *
- * Otherwise, simply argc would be enough:
- *      printf("%d\n", (argc - 1));
+ * Then remembered the introduction of how to void a variable, so:
  */
-	int n = (((argv + argc) - argv) - 1);
-
-	printf("%d\n", n);
+	(void)*argv;
+	printf("%d\n", (argc - 1));
 	return (0);
 }
