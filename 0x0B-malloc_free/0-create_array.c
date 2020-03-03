16599367200048663dbf08937c4126fc389ad82a
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-03 23:12:55 +0000

    now with return values for size == 0 and NULL pointer

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *p;
 
+	if (size == 0)
+		return (NULL);
+
 	p = malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		p[i] = c;
 
-	return (p);
+	if (p == NULL)
+		return (NULL);
+	else
+		return (p);
 }
