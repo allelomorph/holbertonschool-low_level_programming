a141bc5b341e8815175de6bbc1b4cd778bb7e719
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-04 03:10:15 +0000

    straightforward after solving free check on task 3

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees memory allocated to a 2 dimensional array
+ * previously created by the alloc_grid function
+ *
+ * @grid: pointer to 2 dimensional array created by alloc_grid
+ *
+ * @height: amount of arrays arrayed to grid**, also amount of
+ * values printed in each column
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+        }
+	free(grid);
+}
