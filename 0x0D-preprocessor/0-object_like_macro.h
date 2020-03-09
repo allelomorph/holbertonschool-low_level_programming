85e2d9667cea9a4d91d31cab43afb23f7bea9938
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-09 17:39:51 +0000

    first push, need to add 2-main.c

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECTLIKEMACRO_H
+#define OBJECTLIKEMACRO_H
+
+#define SIZE 1024
+
+#endif /* OBJECTLIKEMACRO_H */
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONLIKEMACRO_H
+#define FUNCTIONLIKEMACRO_H
+
+#define ABS(X) ((X) >= 0 ? (X) : -(X))
+
+#endif /* FUNCTIONLIKEMACRO_H */
