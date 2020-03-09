7d2dd332f9e5ec74a1d776b3a8bab99f5fd44359
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-09 23:27:54 +0000

    added string duplication and null checks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+
+int _strlen(char *s);
+char *_strdup(char *str);
 
 /**
  * new_dog - creates a new struct of type dog
@@ -16,17 +18,84 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *nd_name;
+	char *nd_owner;
 	struct dog *nd;
 
-/*	printf("%lu\n", sizeof(struct dog)); */
 	nd = malloc(sizeof(struct dog));
-
 	if (nd == NULL)
 		return (NULL);
 
-	nd->name = name;
+	nd_name = _strdup(name);
+	if (nd_name == NULL)
+	{
+		free(nd);
+		return (NULL);
+	}
+
+	nd_owner = _strdup(owner);
+	if (nd_owner == NULL)
+	{
+		free(nd_name);
+		free(nd);
+		return (NULL);
+	}
+
+	nd->name = nd_name;
 	nd->age = age;
-	nd->owner = owner;
+	nd->owner = nd_owner;
 
 	return (nd);
+}
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string to be measured
+ *
+ * Return: amount of chars in string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	for (; *s; s++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string
+ * given as a parameter.
+ *
+ * @str: string to be copied and used to determine size of
+ * memory allocation
+ *
+ * Return: pointer to first address in the space created
+ * in memory
+ */
+
+char *_strdup(char *str)
+{
+	int size;
+	int i;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = (_strlen(str) + 1);
+
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		p[i] = str[i];
+
+	return (p);
 }
