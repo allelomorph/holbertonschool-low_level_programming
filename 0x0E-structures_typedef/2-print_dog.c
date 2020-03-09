0a84d37e6b0740ed5cd3013dc6e946daa1ecb934
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-09 22:47:09 +0000

    null checks for pointer, omit age check for task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,22 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
 	printf("Name: ");
 	if (d->name == NULL)
 		printf("(nil)\n");
 	else
 		printf("%s\n", d->name);
 
-	printf("Age: ");
-	if (d->age <= 0)
-		printf("(nil)\n");
-	else
-		printf("%f\n", d->age);
+/*
+ *      printf("Age: ");
+ *	if (d->age <= 0)
+ *		printf("(nil)\n");
+ *	else
+ *	printf("%f\n", d->age);
+ */
+	printf("Age: %f\n", d->age);
 	printf("Owner: ");
 	if (d->owner == NULL)
 		printf("(nil)\n");
