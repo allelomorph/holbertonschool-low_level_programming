afb988f3fcb2dca861ed8cb64b72419d9f91fbbd
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-09 22:52:22 +0000

    removed commented out statements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,14 +19,8 @@ void print_dog(struct dog *d)
 	else
 		printf("%s\n", d->name);
 
-/*
- *      printf("Age: ");
- *	if (d->age <= 0)
- *		printf("(nil)\n");
- *	else
- *	printf("%f\n", d->age);
- */
 	printf("Age: %f\n", d->age);
+
 	printf("Owner: ");
 	if (d->owner == NULL)
 		printf("(nil)\n");
