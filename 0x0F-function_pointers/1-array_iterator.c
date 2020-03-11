16127320ac2b6641e2003e2e6536ecbbe6cd67fd
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-11 19:08:39 +0000

    first draft for tasks 0-2, empty files for task 3

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - steps through array "array" of "size" members,
+ * performing "action" function on each
+ *
+ * @array: int array to act on
+ *
+ * @size: amount of members in array
+ *
+ * @action: code executed for each member of "array"
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array to be searched
+ *
+ * @size: amount of elements in "array"
+ *
+ * @cmp: pointer to function that compares values
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
