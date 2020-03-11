29cc0baef2792924f86bf748c54ee93402e2b621
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-11 21:29:27 +0000

    removed else after return for betty style

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		return;
-	else
-		for (i = 0; i < size; i++)
-			action(array[i]);
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
