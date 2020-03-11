4d0f3e5edcd576aa71147c4485c979d67e4b5260
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-11 19:08:39 +0000

    first draft for tasks 0-2, empty files for task 3

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - applies name printing function f to string name
+ *
+ * @name: string containing name
+ *
+ * @f: pointer to function that formats and prints name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
