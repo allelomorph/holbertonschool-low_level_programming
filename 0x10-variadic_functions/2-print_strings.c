3f25abdb9ae8391c04ae0b40747073609a9f1000
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-15 02:30:11 +0000

    slight change to newline logic

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -36,11 +36,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 			}
 		}
-		else
-		{
-			printf("\n");
-		}
 	}
-
+	printf("\n");
 	va_end(args);
 }
