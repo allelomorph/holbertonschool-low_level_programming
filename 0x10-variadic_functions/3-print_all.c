120e39b0328853175a74cc35b861fa39c8829451
Samuel Pomeroy <1960@holbertonschool.com>
2020-03-15 02:16:28 +0000

    first locally successful ver after many drafts

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,85 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything passed as an argument
+ *
+ * @format: list of types of arguments passed to function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	int j;
+
+	char *sep = "";
+
+	p_forms pf_fmt[] = {
+		{"c", (*print_char)},
+		{"i", (*print_int)},
+		{"f", (*print_float)},
+		{"s", (*print_string)},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (pf_fmt[j].f_sym)
+		{
+			if (format[i] == *(pf_fmt[j].f_sym))
+			{
+				printf("%s", sep);
+				pf_fmt[j].p_func(args);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
+
+/**
+ * print_char - prints char passed as argument to variable list
+ */
+
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints integer passed as an argument to variable list
+ */
+
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints floating point passed as an argument to variable list
+ */
+
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints string passed as an argument to variable list
+ */
+
+void print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	if (s == NULL)
+		s = "(nil)";
+
+	printf("%s", s);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,20 +5,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct print_formats - Struct p_forms
- *
- * @f_sym: format symbol given to main
- * @pf_tag: format tag passed as string to printf
- * @p_func: pointer to corresponding helper function
- */
-typedef struct print_formats
-{
-	char f_sym;
-	char *pf_tag;
-	void (*p_func)(va_list);
-} p_forms;
-
 int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
@@ -28,5 +14,26 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+/**
+ * struct print_formats - Struct p_forms
+ *
+ * @f_sym: format symbol given to main
+ * @pf_tag: format tag passed as string to printf
+ * @p_func: pointer to corresponding helper function
+ */
+typedef struct print_formats
+{
+	char *f_sym;
+	void (*p_func)(va_list);
+} p_forms;
+
+void print_char(va_list args);
+
+void print_int(va_list args);
+
+void print_float(va_list args);
+
+void print_string(va_list args);
 
 #endif /* VARIADIC_FUNCTIONS_H */
