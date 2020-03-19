29c5294b6ac4eef5702c0bef4f62c99b83140fd1
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-19 21:26:44 +0000

    added NULL check for first pointer passed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,20 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int count = 1;
+	unsigned int nodes = 1;
 
-	if (!h->str)
-		printf("[0] (nil)\n");
+	if (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		if (h->next)
+			nodes += print_list(h->next);
+
+		return (nodes);
+	}
 	else
-		printf("[%d] %s\n", h->len, h->str);
-
-	if (h->next)
-		count += print_list(h->next);
-
-	return (count);
+		return (0);
 }
