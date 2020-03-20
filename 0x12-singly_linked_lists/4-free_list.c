c8ae8bfb8f8798bf27ed16637bb1f2f403da2b61
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-20 05:22:47 +0000

    first version to pass valgrind

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_list - frees memory allocated to a struct type
+ * list_t linked list
+ *
+ * @head: pointer to first member of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (!head)
+		return;
+
+	while(head != NULL)
+	{
+/* point to current head */
+		temp = head;
+/* current head moves one member forward in list */
+		head = head->next;
+/* free string and stuct ptr alloc for previous head */
+		free(temp->str);
+		free(temp);
+	}
+}
