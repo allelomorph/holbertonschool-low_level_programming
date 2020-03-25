441d5f80faa8f66442da2631508f8c1445d2759b
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-25 01:52:14 +0000

    added NULL check of *head

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -72,7 +72,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	size_t length;
 	listint_t *temp;
 
-	if (!head)
+	if (!head || !(*head))
 		return (-1);
 
 	length = listint_len(*head);
