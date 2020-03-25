6ce28cfd44220f0162de9cc50e3e8caf08899049
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-25 00:37:43 +0000

    leveraged helpers, is there a more elegant way?

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,59 @@
 #include "lists.h"
 
 /**
+ * listint_len - counts all the elements of a struct list type listint_t
+ *
+ * @h: pointer to the head/first member of a singly linked list of structs
+ * of type listint_s
+ *
+ * Return: amount of nodes in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 1;
+
+	if (h)
+	{
+		if (h->next)
+			count += listint_len(h->next);
+
+		return (count);
+	}
+	else
+		return (0);
+}
+
+/**
+ * add_nodeint - adds a new node to the start of a struct list type listint_t
+ *
+ * @head: double pointer to the first member of a singly linked list
+ * of structs of type listint_s
+ *
+ * @n: int to store in n member of new listint_s struct
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_n;
+
+	new_n = malloc(sizeof(listint_t));
+	if (!new_n)
+	{
+		free(new_n);
+		return (NULL);
+	}
+
+	new_n->n = n;
+	new_n->next = *head;
+	*head = new_n;
+
+	return (new_n);
+}
+
+/**
  * insert_nodeint_at_index - inserts a new node into a listint_t linked list
  * at index position idx with n value
  *
@@ -16,6 +69,8 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+/* can it be done with less helpers? try refactoring later */
+	size_t list_len;
 	listint_t *new_n;
 	listint_t *temp = *head;
 	unsigned int i;
@@ -23,32 +78,32 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head)
 		return (NULL);
 
+	list_len = listint_len(*head);
+
+	if (idx > list_len)
+		return (NULL);
+
+	if (idx == list_len)
+		return (add_nodeint_end(head, n));
+
+	if (idx == 0)
+		return (add_nodeint(head, n));
+
 	new_n = malloc(sizeof(listint_t));
 	if (!new_n)
 		return (NULL);
 
-	new_n->n = n;
-
-	if (*head)
+	if (!*head)
 	{
-		if (idx == 0)
-		{
-			new_n->next = temp;
-			*head = new_n;
-			return (new_n);
-		}
-		for (i = 0; temp->next; i++)
-		{
-			if (i == (idx - 1))
-			{
-				new_n->next = temp->next;
-				temp->next = new_n;
-				return (new_n);
-			}
-			else
-				temp = temp->next;
-		}
+		free(new_n);
+		return (NULL);
 	}
-	free(new_n);
-	return (NULL);
+	for (i = 0; (i < (idx - 1)); i++)
+	{
+		temp = temp->next;
+	}
+	new_n->n = n;
+	new_n->next = temp->next;
+	temp->next = new_n;
+	return (new_n);
 }
