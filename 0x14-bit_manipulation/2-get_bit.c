78e8277da91067a130373b1d9d6761f014b52c0d
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-27 00:44:38 +0000

    second attempt at ULONG_MAx protection

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,21 +7,21 @@
  *
  * @index: position in binary notation to retrieve. index in this case
  * counts from 0 at rightmost printed digit, or 2^0.
+ *
+ * Return: integer value of bianry digit at position "index"
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int len = 0;
 
-	if (n == ULONG_MAX)
+	if (n > LONG_MAX)
 	{
-		if (index != 63)
-		{
-			len++;
-			n = LONG_MAX;
-		}
-		else
+		if (index == 63)
 			return (1);
+
+		len++;
+		n =  (n - LONG_MAX) - 1;
 	}
 
 /* length of binary notation in chars, with leading zeroes truncated */
@@ -30,7 +30,7 @@ int get_bit(unsigned long int n, unsigned int index)
 /* minus 1 to match the way binary powers of 2 start at rightmost digit, 2^0 */
 	len--;
 	if (index > (unsigned int)len)
-		return (-1);
+	return (-1);
 
 	return ((n >> index) & 1);
 }
