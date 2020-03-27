1479a27d04279c11ed5c1f96dde5235c518d7e0e
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-27 05:48:39 +0000

    added support up to ULONG_MAX

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,26 +9,42 @@
 
 void print_binary(unsigned long int n)
 {
-	int len;
-
+	int len, i;
+	long int temp;
 	if (n == 0)
 		_putchar('0');
 
-	if (n == ULONG_MAX)
+	if (n >= (unsigned long int)LONG_MIN)
 	{
+		n -= (unsigned long int)LONG_MIN;
+		temp = (long int)n;
 		_putchar('1');
-		n = LONG_MAX;
+		for (i = 62; i > -1; i--)
+		{
+/*			if (!((i + 1) % 4))
+				_putchar(' ');
+			if (!((i + 1) % 16))
+			_putchar(' '); */
+			_putchar(((temp >> i) & 1) + '0');
+		}
 	}
-
-/* length of binary notation in chars, with leading zeroes truncated */
-	for (len = 0; (n >> len) | 0; len++)
-	{}
-/* minus 1 to match the way binary powers of 2 start at rightmost digit, 2^0 */
-	len--;
-/* start at leftmost digit to print left to right, and work backwards */
-	for (; len > -1; len--)
+	else
 	{
+		temp = (long int)n;
+/* length of binary notation in chars, with leading zeroes truncated */
+		for (len = 0; (temp >> len) | 0; len++)
+		{}
+/* minus 1 to match the way binary powers of 2 start at rightmost digit, 2^0 */
+		len--;
+/* start at leftmost digit to print left to right, and work backwards */
+		for (; len > -1; len--)
+		{
+/*			if (!((len + 1) % 4))
+				_putchar(' ');
+			if (!((len + 1) % 16))
+			_putchar(' '); */
 /* print n / 2^len. & 1 compares to ..0001 to just return that digit */
-		_putchar(((n >> len) & 1) + '0');
+			_putchar(((temp >> len) & 1) + '0');
+		}
 	}
 }
