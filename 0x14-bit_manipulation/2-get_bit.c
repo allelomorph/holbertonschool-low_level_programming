a60ed8cbfbda4986617359477f6cfbfbddbde0d1
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-27 07:06:25 +0000

    trying to generalize ULONG_MAX support

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,23 +14,25 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int len = 0;
+	long int temp_n;
 
-	if (n > LONG_MAX)
+	if (n >= (unsigned long int)LONG_MIN)
 	{
 		if (index == 63)
 			return (1);
 
 		len++;
-		n -= ((unsigned long int)LONG_MAX + 1);
+		n -= (n >= (unsigned long int)LONG_MIN);
 	}
+	temp_n = (long int)n;
 
 /* length of binary notation in chars, with leading zeroes truncated */
-	for (; (n >> len) | 0; len++)
+	for (; (temp_n >> len) | 0; len++)
 	{}
 /* minus 1 to match the way binary powers of 2 start at rightmost digit, 2^0 */
 	len--;
 	if (index > (unsigned int)len)
 		return (-1);
 
-	return ((n >> index) & 1);
+	return ((temp_n >> index) & 1);
 }
