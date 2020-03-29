75880affba2e62741de3ccecc22efdb65f46b03b
Samuel Pomeroy <1690@holbertonschool.com>
2020-03-29 04:39:49 +0000

    adding 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+#define LITTLE_ENDIAN 1
+#define BIG_ENDIAN 0
+
+/**
+ * get_endianness - determines whether complie environment
+ * is big-endian (MSB pointed to) or little-endian (LSB pointed to)
+ *
+ * Return: 0 if big-endian, 1 if little-endian
+ */
+
+int get_endianness(void)
+{
+	int test_num;
+	char *c_ptr;
+
+	test_num = 1;
+	c_ptr = (char *)&test_num;
+	return (c_ptr[0] ? LITTLE_ENDIAN : BIG_ENDIAN);
+}
