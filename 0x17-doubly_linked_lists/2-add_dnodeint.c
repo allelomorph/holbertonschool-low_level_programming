dd81f39182dd02f068b10edb4ad89298115b7bda
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-03 01:46:19 +0000

    first attempt t2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to first member of a dlistint_t list
+ * @n: int value stored in new node
+ * Return: address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t))_;
+	if (!new)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
+	}
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
