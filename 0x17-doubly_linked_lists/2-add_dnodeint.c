2b4a7ee173dd2f2def6ed56b62a834e3842a4ab1
Samuel Pomeroy <1690@holbertonschool.com>
2020-05-03 01:49:08 +0000

    typo line 14

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t))_;
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 	{
 		dprintf(2, "Error: Can't malloc\n");
