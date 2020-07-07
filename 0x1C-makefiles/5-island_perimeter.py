816b1a8c31e721e92d2753f559b9e5d543ce9f4e
Samuel Pomeroy <1690@holbertonschool.com>
2020-07-07 04:37:08 +0000

    first attempt t5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""0x1C. C - Makefiles, task 5. Island Perimeter
+
+"""
+
+
+def island_perimeter(grid):
+    """Find the perimeter of an "island" of 1s in an "ocean" of 0s.
+
+    For a 2D grid of 0s and 1s, find the perimeter of the contiguous area of
+    1, with negative space in 0s.
+
+    Args:
+        grid (list of lists of ints): 2D list representation of island
+
+    Attributes:
+        perimeter (int): total units of cell length around island edge
+
+    """
+    perimeter = 0
+    for x in range(1, len(grid) - 1):
+        for y in range(1, len(grid[0]) - 1):
+            if grid[x][y]:
+                if not (grid[x][y - 1]):
+                    perimeter += 1
+                if not (grid[x][y + 1]):
+                    perimeter += 1
+                if not (grid[x - 1][y]):
+                    perimeter += 1
+                if not (grid[x + 1][y]):
+                    perimeter += 1
+    return perimeter
