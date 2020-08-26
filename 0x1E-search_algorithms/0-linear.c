853e3004b6fe0fcdd89c10ec95a5e7bf0b0ec101
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-26 00:52:52 +0000

    first attempts t0-6; empty files for advanced

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an integer array using a linear
+ * search algorithm
+ * @array: pointer to first element of array to seach
+ * @size: number of elements in array
+ * @value: value ot search for
+ *
+ * Return: first index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int index = -1;
+
+	if (!array)
+		return (index);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			index = (int)i;
+			break;
+		}
+	}
+
+	return (index);
+}
---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,2 @@
+#include "search_algos.h"
+
